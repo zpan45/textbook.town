94f518d4cb4d17d070e09cadd0015beebe6c0ec4
psaly <psaly@uwo.ca>
2017-02-27 18:43:15 -0500

    Added a new user login in bootstrap

--- TextbookBidding/grails-app/init/BootStrap.groovy
+++ TextbookBidding/grails-app/init/BootStrap.groovy
@@ -6,10 +6,18 @@ import grails3.example.UserRole
 class BootStrap {
 
     def init = { servletContext ->
+
+        // make a generic test account
         def role = new Role(authority: 'ROLE_USER').save()
         def user = new User(username: 'test', password: '2212',
                             profile: new Profile(displayName: 'Philip', contactMethod: 'facebook')).save()
         UserRole.create(user, role, true)
+
+        // make another account
+        def u = new User(username: 'Pierce', password: 'Grails',
+                profile: new Profile(displayName: 'Pierce', contactMethod: 'TWITTER')).save()
+        UserRole.create(u, role, true)
+
 
 
     }

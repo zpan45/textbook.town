92423b1bb755e6fee0c2fcd203f0e8b9c98e04d4
Nick Elder <nickelder@me.com>
2017-03-02 21:36:25 -0500

    Finished reg page.

--- webroot/js/core.js
+++ webroot/js/core.js
@@ -20,3 +20,28 @@ function renderBookList(bl){
 		render("#bookList", "tp/book.html", {title: book.title, price: book.price, date_closing: book.date_closing, subject: book.subject, img: book.image}, "APPEND_MODE")
 	});
 }
+
+//Set Cookie Function
+function setCookie(cname, cvalue, exdays){
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays));
+	var expires = "expires="+ d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+////
+//Fetch cookie by name
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for(var i = 0; i <ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}

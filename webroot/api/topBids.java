9d8b38d6033dd5dabd9bc94d81c62fa243b6fbdf
nbarucha <n.barucha@gmail.com>
2017-03-20 21:58:58 -0400

    top 3 bid information when function is successful

---
+++ webroot/api/topBids.java
@@ -0,0 +1,20 @@
+{
+  "status": "success",
+  "bids": [
+  		{
+        "bid": 120
+        "user_name": "nicole"
+        "profile_link": "dsjkbvihbvjabshj"
+  		},
+  		{
+        "bid": 115
+        "user_name": "nick"
+        "profile_link": "jkvbhjbvjhbahjvb"
+  		}
+      {
+        "bid": 110
+        "user_name": "jeremy"
+        "profile_link": "kajbhwbvwjabjk"
+      }
+  ]
+}
